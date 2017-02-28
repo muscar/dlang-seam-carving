248d1070861c14a74c607c8911fa02a6de1f2ed6
Alex Muscar <alex.muscar@ly.st>
2017-02-28 16:16:40 +0000

    Combine channel edges into image edge

--- source/app.d
+++ source/app.d
@@ -21,8 +21,8 @@ auto at(MemoryImage img, int x, int y)
 auto sobel(MemoryImage img)
 {
     auto edge = new TrueColorImage(img.width, img.height);
-    foreach (int x; 1..img.width - 1) {
-        foreach (int y; 1..img.height - 1) {
+    foreach (x; 1..img.width - 1) {
+        foreach (y; 1..img.height - 1) {
             auto px = (sobelX[0][0] * img.at(x - 1, y - 1)) + (sobelX[0][1] * img.at(x, y - 1)) + (sobelX[0][2] * img.at(x + 1, y - 1)) +
                       (sobelX[1][0] * img.at(x - 1, y))   + (sobelX[1][1] * img.at(x, y))   + (sobelX[1][2] * img.at(x + 1, y)) +
                       (sobelX[2][0] * img.at(x - 1, y + 1)) + (sobelX[2][1] * img.at(x, y + 1)) + (sobelX[2][2] * img.at(x + 1, y + 1));
@@ -43,8 +43,8 @@ void main(string[] args)
     auto gimg = new TrueColorImage(img.width, img.height);
     auto bimg = new TrueColorImage(img.width, img.height);
 
-    foreach (int x; 0..img.width) {
-        foreach (int y; 0..img.height) {
+    foreach (x; 0..img.width) {
+        foreach (y; 0..img.height) {
             auto c = img.getPixel(x, y);
             rimg.setPixel(x, y, Color(c.r, c.r, c.r));
             gimg.setPixel(x, y, Color(c.g, c.g, c.g));
@@ -56,11 +56,21 @@ void main(string[] args)
     auto gedge = sobel(gimg);
     auto bedge = sobel(bimg);
 
-    writePng("r.png", rimg);
-    writePng("g.png", gimg);
-    writePng("b.png", bimg);
+    auto edge = new TrueColorImage(img.width, img.height);
+    foreach (x; 0..img.width) {
+        foreach (y; 0..img.height) {
+            auto val = redge.getPixel(x, y).r + gedge.getPixel(x, y).g + bedge.getPixel(x, y).b;
+            edge.setPixel(x, y, Color(val, val, val));
+        }
+    }
 
-    writePng("redge.png", redge);
-    writePng("gedge.png", gedge);
-    writePng("bedge.png", bedge);
+    writePng("out/r.png", rimg);
+    writePng("out/g.png", gimg);
+    writePng("out/b.png", bimg);
+
+    writePng("out/redge.png", redge);
+    writePng("out/gedge.png", gedge);
+    writePng("out/bedge.png", bedge);
+
+    writePng("out/edge.png", edge);
 }
