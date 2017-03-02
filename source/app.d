e4d07f335fd8ba6dfc5edabf814f87aeccd77148
Alex Muscar <alex.muscar@ly.st>
2017-03-02 13:08:19 +0000

    Clean up code

--- source/app.d
+++ source/app.d
@@ -1,21 +1,22 @@
 import std.algorithm.comparison;
-import std.stdio;
+import std.conv;
 import std.math;
+import std.stdio;
 import std.typecons;
 
 import arsd.png;
 
-auto sobelX = [[-1, 0, 1],
-               [-2, 0, 2],
-               [-1, 0, 1]];
+immutable auto sobelX = [[-1, 0, 1],
+                         [-2, 0, 2],
+                         [-1, 0, 1]];
  
-auto sobelY = [[-1, -2, -1],
-               [ 0,  0,  0],
-               [ 1,  2,  1]];
+immutable auto sobelY = [[-1, -2, -1],
+                         [ 0,  0,  0],
+                         [ 1,  2,  1]];
 
 auto at(MemoryImage img, int x, int y)
 {
-    return img.getPixel(x, y).toBW.r;
+    return img.getPixel(x, y).components[0];
 }
 
 auto decompose(MemoryImage img)
@@ -36,29 +37,34 @@ auto decompose(MemoryImage img)
     return tuple!("red", "green", "blue")(rimg, gimg, bimg);
 }
 
-auto filter3x3(S)(MemoryImage image, int[][] kernel, S s)
+auto filter3x3(S)(MemoryImage image, immutable int[][] kernel, S s)
 {
     auto res = new TrueColorImage(image.width, image.height);
     foreach (x; 1..image.width - 1) {
         foreach (y; 1..image.height - 1) {
-            int[3] acc = [0, 0, 0];
+            int[4] acc = [0, 0, 0, 255];
             foreach (ky; 0..3) {
                 foreach (kx; 0..3) {
-                    import std.format;
-                    auto p = image.getPixel(x + kx - 1, y + ky - 1);
-                    auto k = kernel[ky][kx];
-                    acc[0] += (p.r * k);
-                    acc[1] += (p.g * k);
-                    acc[2] += (p.b * k);
+                    accumulate(acc, image.getPixel(x + kx - 1, y + ky - 1).components, kernel[ky][kx]);
                 }
             }
-            res.setPixel(x, y, Color(s(acc[0]), s(acc[1]), s(acc[2])));
+            foreach (ref c; acc) {
+                c = s(c);
+            }
+            res.setPixel(x, y, Color.fromComponents(acc.to!(ubyte[4])));
         }
     }
     return res;
 }
 
-auto gradients(MemoryImage image, int[][] hKernel, int[][] vKernel)
+auto accumulate(ref int[4] acc, immutable ubyte[4] c, int k)
+{
+    acc[0] += c[0] * k;
+    acc[1] += c[1] * k;
+    acc[2] += c[2] * k;
+}
+
+auto gradients(MemoryImage image, immutable int[][] hKernel, immutable int[][] vKernel)
 {
     auto res = new TrueColorImage(image.width, image.height);
     auto hGrad = filter3x3(image, hKernel, (int v) => clamp(v, 0, 255));
@@ -74,21 +80,21 @@ auto gradients(MemoryImage image, int[][] hKernel, int[][] vKernel)
     return res;
 }
 
-auto sobel(MemoryImage img)
+auto sobelGradients(MemoryImage image)
 {
-    auto edge = new TrueColorImage(img.width, img.height);
-    foreach (x; 1..img.width - 1) {
-        foreach (y; 1..img.height - 1) {
-            auto px = (sobelX[0][0] * img.at(x - 1, y - 1)) + (sobelX[0][1] * img.at(x, y - 1)) + (sobelX[0][2] * img.at(x + 1, y - 1)) +
-                      (sobelX[1][0] * img.at(x - 1, y))   + (sobelX[1][1] * img.at(x, y))   + (sobelX[1][2] * img.at(x + 1, y)) +
-                      (sobelX[2][0] * img.at(x - 1, y + 1)) + (sobelX[2][1] * img.at(x, y + 1)) + (sobelX[2][2] * img.at(x + 1, y + 1));
-            auto py = (sobelY[0][0] * img.at(x - 1, y - 1)) + (sobelY[0][1] * img.at(x, y - 1)) + (sobelY[0][2] * img.at(x + 1, y - 1)) +
-                      (sobelY[1][0] * img.at(x - 1, y))   + (sobelY[1][1] * img.at(x, y))   + (sobelY[1][2] * img.at(x + 1, y)) +
-                      (sobelY[2][0] * img.at(x - 1, y + 1)) + (sobelY[2][1] * img.at(x, y + 1)) + (sobelY[2][2] * img.at(x + 1, y + 1));
-            px = clamp(px, 0, 255);
-            py = clamp(py, 0, 255);
-            auto val = cast(int)clamp(sqrt(cast(float)(px * px) + cast(float)(py * py)).ceil, 0, 255);
-            edge.setPixel(x, y, Color(val, val, val).toBW);
+    return gradients(image, sobelX, sobelY);
+}
+
+auto combine(MemoryImage redge, MemoryImage gedge, MemoryImage bedge)
+{
+    assert(redge.width == bedge.width && bedge.width == gedge.width);
+    assert(redge.width == bedge.width && bedge.width == gedge.width);
+
+    auto edge = new TrueColorImage(redge.width, redge.height);
+    foreach (x; 0..redge.width) {
+        foreach (y; 0..redge.height) {
+            auto val = clamp(redge.at(x, y) + gedge.at(x, y) + bedge.at(x, y), 0, 255);
+            edge.setPixel(x, y, Color(val, val, val));
         }
     }
     return edge;
@@ -97,30 +103,12 @@ auto sobel(MemoryImage img)
 void main(string[] args)
 {
     auto img = readPng(args[1]);
-    auto channels = decompose(img);
-    auto redge = sobel(channels.red);
-    auto gedge = sobel(channels.green);
-    auto bedge = sobel(channels.blue);
+    auto channels = img.decompose;
 
-    auto redge1 = gradients(channels.red, sobelX, sobelY);
-    auto gedge1 = gradients(channels.green, sobelX, sobelY);
-    auto bedge1 = gradients(channels.blue, sobelX, sobelY);
-
-    auto bwImg = new TrueColorImage(img.width, img.height);
-    foreach (x; 0..img.width) {
-        foreach (y; 0..img.height) {
-            bwImg.setPixel(x, y, img.getPixel(x, y).toBW);
-        }
-    }
-    auto imgEdge = sobel(bwImg);
-
-    auto edge = new TrueColorImage(img.width, img.height);
-    foreach (x; 0..img.width) {
-        foreach (y; 0..img.height) {
-            auto val = clamp(redge.at(x, y) + gedge.at(x, y) + bedge.at(x, y), 0, 255);
-            edge.setPixel(x, y, Color(val, val, val).toBW);
-        }
-    }
+    auto redge = channels.red.sobelGradients;
+    auto gedge = channels.green.sobelGradients;
+    auto bedge = channels.blue.sobelGradients;
+    auto edge = combine(redge, gedge, bedge);
 
     writePng("out/r.png", channels.red);
     writePng("out/g.png", channels.green);
@@ -130,10 +118,5 @@ void main(string[] args)
     writePng("out/gedge.png", gedge);
     writePng("out/bedge.png", bedge);
 
-    writePng("out/redge1.png", redge1);
-    writePng("out/gedge1.png", gedge1);
-    writePng("out/bedge1.png", bedge1);
-
     writePng("out/edge.png", edge);
-    writePng("out/img_edge.png", imgEdge);
 }
